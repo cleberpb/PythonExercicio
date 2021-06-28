a4019e84d4fa077fa3b91f10194716ae505f02b5
cleberpb <cleberppio@gmail.com>
2021-06-27 21:10:59 -0300

    atualizar programas

---
+++ ex042.py
@@ -0,0 +1,11 @@
+print('-=-'*15)
+print('Analizador de um Triângulo')
+print('-=-'*15)
+primeiro = float(input('Digite o primeiro número: '))
+segundo = float(input('Digite o segundo número: '))
+terceiro = float(input('Digite o terceiro número: '))
+
+if primeiro < segundo + terceiro and segundo < primeiro + terceiro and terceiro < primeiro + segundo:
+    print('Os seguimentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os segmentos acima NÂO PODEM FORMAR triângulo!')
