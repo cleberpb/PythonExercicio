0ad92e3055a2a32522df84dce00c0a4f187bf305
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-06-30 09:24:59 -0300

    Update ex037.py
    O código do professor ficou bem merlhor, mesmo o meu estando funcionando

--- ex037.py
+++ ex037.py
@@ -13,6 +13,8 @@ print('1 - Binário | 2 - Octal | 3 - Hexadecimal')
 opcao = int(input('Escolha uma opçãp: '))
 print('------------------------------------------')
 
+'''
+Eu fiz este pedaço de código - está funcionando
 a1 = ('{0:b}'.format(numero))
 b1 = ('{:o}'.format(numero))
 c1 = ('{0:x}'.format(numero))
@@ -23,5 +25,14 @@ elif opcao == 2:
     print('Em Octal é {}.'.format(b1))
 elif opcao == 3:
     print('Em Hexadecimal é {}.'.format(c1))
+else:
+    print('\nOpção inválida! Tente novamente.')'''
+
+if opcao == 1:
+    print('{} convertido para BINÁRIO é igual a {}.'.format(numero, bin(numero)[2:]))
+elif opcao == 2:
+    print('{} convertido para OCTAL é igual a {}.'.format(numero, oct(numero)[2:]))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}.'format(numero, hex(numero)[2:]))
 else:
     print('\nOpção inválida! Tente novamente.')
