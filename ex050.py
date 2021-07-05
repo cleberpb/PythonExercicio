61fe4718da4ef674904bd8a61aa8c5fd67734ae2
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-07-05 13:42:38 -0300

    Update ex050.py
    Somando números pares

--- ex050.py
+++ ex050.py
@@ -3,3 +3,10 @@ Desenvolva um programa que leia seis números inteiros
 e mostre a soma apenas daqueles que forem pares. Se o
 valor for ímpar, desconsidere-o.
 '''
+
+total = 0
+for somar in range(1, 7):
+    numero = int(input('Digite um número: '))
+    if numero % 2 == 0:
+        total = total + numero
+print(f'Total dos números pares são {total}.')
