c624ae79822183064fde28bfa758e860b9a21f8c
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-07-05 19:33:41 -0300

    Update ex046.py
    Contagem regressiva

--- ex046.py
+++ ex046.py
@@ -3,3 +3,38 @@ Faça um programa que mostre na tela uma contagem egressiva para
 o estouro de fogos de artifício, indo de 10 até 0, com uma pausa
 de 1 segundo entre eles.
 '''
+'''
+Faça um programa que mostre na tela uma contagem regressiva para
+o estouro de fogos de artifício, indo de 10 até 0, com uma pausa
+de 1 segundo entre eles.
+'''
+import time
+print('-='*13)
+print('FESTEJOS DE FOGOS DE ARTIFÍCIOS')
+print('-='*13)
+print('\nCONTAGEM REGRESSIVA')
+for contagem in range(10, 0, -1):
+    time.sleep(1)
+    print(contagem)
+print('''
+* * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * *
+  * * * * * * * * * * * * * * * * * *
+   * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * * *
+     * * * * * * * * * * * * * * *
+      * * * * * * * * * * * * * *
+       * * * * * * * * * * * * *
+        * * * * * * * * * * * *
+         * * * * * * * * * * *
+          * * * * * * * * * *
+           * * * * * * * * *
+            * * * * * * * * 
+             * * * * * * *
+              * * * * * *
+               * * * * *
+                * * * *
+                 * * *
+                  * *
+                   *
+''')
