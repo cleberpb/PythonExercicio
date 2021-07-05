646d6c7d27adad1d079ae213dd5d507d3e8e9f54
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-07-05 11:54:01 -0300

    Update ex048.py
    somando múltiplos de 3.

--- ex048.py
+++ ex048.py
@@ -1,3 +1,6 @@
 '''Faça um programa que calcula e soma entre todos os
 números impares que são múltiplos de três e que se
 encontram no intervalo de 1 até 500.'''
+
+total = sum(range(3, 500, 3))
+print(f'A soma dos números múltiplos de três de 1 a 500 é: {total}.')
