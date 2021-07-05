dc446db426468cdbb5ce0d5ddb3e5d04aa69b3d5
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-07-05 09:51:17 -0300

    Update ex049.py
    Refazendo tabuada com for

--- ex049.py
+++ ex049.py
@@ -3,3 +3,12 @@ Refaça o Desafio ex09, mostrando a tabuada de um número
 que o usuário escolher, só que agora utilizando um
 laço for.
 '''
+
+print('-='*18)
+print('TABUADA')
+print('-='*18)
+numero = int(input('Digite um número para a tabuada: '))
+print(f'Segue abaixo a tabuada de {numero}.\n')
+for tabuada in range(1, 11):
+    resultado = numero * tabuada
+    print('{} x {} = {}'.format(tabuada, numero, resultado))
