f1f1dd35c397b729474acd1219deaae1b49b3cf1
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-07-12 12:59:10 -0300

    Update ex051.py

--- ex051.py
+++ ex051.py
@@ -3,3 +3,14 @@ Desenvolva um programa que leia o primeiro termo e a
 razão de uma PA(Progressão Aritmética). No final,
 mostre os 10 primeiros termos desta progressão.
 '''
+
+
+primeiro_termo = int(input('Digite o pimeiro termo: '))
+segundo_termo = int(input('Digite o segunto termo: '))
+razao_pa = int(input('Digite a razão: '))
+
+for progressao in range(primeiro_termo, segundo_termo, razao_pa):
+    print(progressao)
+print(f'O primeiro termo é {primeiro_termo}.')
+print(f'O Segundo termo é {segundo_termo}.')
+print(f'A razão é {razao_pa}.')
