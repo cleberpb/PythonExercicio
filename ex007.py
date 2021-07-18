4cc38addd4c64a692b3ee7ecce3fa5f052dffb86
cleberpb <cleberppio@gmail.com>
2021-07-18 12:07:08 -0300

    atualizado

--- ex007.py
+++ ex007.py
@@ -4,4 +4,4 @@
 nota1 = float(input('Digite a primeira nota do aluno(a): '))
 nota2 = float(input('Digite a segunda nota do aluno(a): '))
 media = (nota1 + nota2) / 2
-print('Você tirou as nostas {} e {}, a sua média é {:.2f}'.format(nota1, nota2, media))
+print('Você tirou as nostas {} e {}, a sua média é {:.2f}.'.format(nota1, nota2, media))
