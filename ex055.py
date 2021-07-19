99abb4d4fc116b7d90c65b7c364037df133028ae
cleberpb <cleberppio@gmail.com>
2021-07-19 18:05:37 -0300

    anailizar

--- ex055.py
+++ ex055.py
@@ -2,3 +2,13 @@
 Faça um programa que leia o peso de cisco pessoas.
 No final, mostre qual foi o maior e o menor peso lidos.
 '''
+maior_peso = 0
+menor_peso = 0
+for pessoa_peso in range(1, 6):
+    peso = float(input('Qual é o seu peso? '))
+    if peso < maior_peso:
+        menor_peso = menor_peso + peso
+    if peso > menor_peso:
+       maior_peso = maior_peso + peso
+print(f'O menor peso é {menor_peso}')
+print(f'O maior peso é {maior_peso}')
--- ex056.py
+++ ex056.py
@@ -5,3 +5,17 @@ Desenvolva um programa que leia o nome, idade e sexo de
 - Qual é o nome do homem mais velho.
 - Quantas mulheres têm menos de 20 anos.
 '''
+
+media_idade = 0
+mais_velho = 0
+menoridade = 0
+
+for pessoas in range(1, 5):
+    nome = input('Qual é o seu nome? ')
+    idade = int(input('Quantos anos você tem? '))
+    sexo = input('Sexo: M ou F? ')
+    media_idade = media_idade + idade
+    
+print(media_idade / 4)
+
+
