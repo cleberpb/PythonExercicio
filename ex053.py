09d3eda272c1b2f5aa6a1f4d0ae84ff1faa699da
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-07-19 14:08:44 -0300

    Update ex053.py

--- ex053.py
+++ ex053.py
@@ -9,3 +9,19 @@ Ex:
 - O LOBO AMA O BOLO
 - ANOTARAM A DATA DA MARATONA
 '''
+
+print('-='*25)
+print('''Escreva uma frase, e veja se ela pode ser lida 
+de trás para ferente. OBS: deve ser sem acento.''')
+print('-='*25)
+frase = input('\nEscreva uma frase: \n').lower()
+juntar = ''.join(frase.split())
+print(f'Você escreveu a frase {frase}.\n')
+inverter = juntar[::-1]
+
+if juntar == inverter:
+    print(f'Está frase lida de trás para frete fica do mesmo jeito.')
+    print('Ela é um Políndromo.')
+else:
+    print(f'Está frase lida de trás para frente fica muito doida.')
+    print('Ela NÃO é um políndromo. ')
