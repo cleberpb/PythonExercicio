acc03ebb4400362bfdcb1eb68bb2aa1109c86a02
cleberpb <cleberppio@gmail.com>
2021-07-19 20:30:53 -0300

    maioridade e menoridade

--- ex054.py
+++ ex054.py
@@ -4,3 +4,16 @@ No final, mostre quantas pessoas ainda não atingiram a maioridade
 e quantas já são maiores.
 Considerando a maioridade 21 anos.
 '''
+menoridade = 0
+maioridade = 0
+for pessoas in range(1, 8):
+    ano = int(input('Quantos anos você tem? '))
+    if ano <= 20:
+        menoridade = menoridade + 1
+    if ano >= 21:
+        maioridade = maioridade + 1
+print(f'{menoridade} pessoa(as) tem menoridade.')
+print(f'{maioridade} pessoa(as) tem maioridade.')
+
+   
+
