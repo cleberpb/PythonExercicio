0d1d11be7c7ba44b1153671f7f820538f68a6418
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-07-20 12:59:49 -0300

    resolvido - maior e menor peso

--- ex055.py
+++ ex055.py
@@ -2,13 +2,18 @@
 Faça um programa que leia o peso de cisco pessoas.
 No final, mostre qual foi o maior e o menor peso lidos.
 '''
+
 maior_peso = 0
 menor_peso = 0
-for pessoa_peso in range(1, 6):
-    peso = float(input('Qual é o seu peso? '))
-    if peso < maior_peso:
-        menor_peso = menor_peso + peso
-    if peso > menor_peso:
-       maior_peso = maior_peso + peso
-print(f'O menor peso é {menor_peso}')
-print(f'O maior peso é {maior_peso}')
+for pessoa in range(1, 6):
+    peso = float(input(f'Peso da {pessoa}ª pessoa: '))
+    if pessoa == 1:
+        maior_peso = peso
+        menor_peso = peso
+    else:
+        if peso > maior_peso:
+            maior_peso = peso
+        if peso < peso:
+            menor = peso
+print(f'O maior peso lido foi {maior_peso}kg.')
+print(f'O menor peso lido foi {menor_peso}kg.')
