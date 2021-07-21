3f4ff71a51c33ccf191b4ee943fdd17c405aefc2
cleberpb <cleberppio@gmail.com>
2021-07-21 20:21:25 -0300

    resolvendo

--- ex057.py
+++ ex057.py
@@ -3,3 +3,14 @@ Faça um programa que leia o sexo de uma pessoa, mas só aceite
 os valores 'M' ou 'F'. Caso esteja errada, peça a condição
 novamente até ter um valor correto.
 '''
+
+m = 'm'
+f = 'f'
+sexo = ''
+
+while sexo != m and sexo != f:
+    sexo = str(input('\nDigite o sexo [ M / F ]: ')).lower()
+    print('O sexo deve ser M ou F. Digite novamente.')
+    print('-='*21)
+    
+print(f'O sexo digitado foi {sexo.upper()}.\n')
--- ex058.py
+++ ex058.py
@@ -2,5 +2,27 @@
 Melhore o jogo do desafio 028 onde o computador vai "pensar" em um
 número entre 0 e 10. Só que agora o jogador vai tentar adivinhar
 até acertar, mostrando no final quantos palpites foram necessários
-para vencar.
+para vencer.
 '''
+
+import random
+
+
+print('\n*************************************************************************')
+print('VAMOS JOGAR? Eu vou pensar em um número de 0 a 10, e você deve adivinhar.')
+print('*************************************************************************')
+
+contador = 0
+numero_chutado = 0
+computaddor = random.choice([1,2,3,4,5,6,7,8,9,10])
+
+while numero_chutado != computaddor :    
+    numero_chutado = int(input('Chute um número de 1 a 10: '))
+    print(f'ERROU! Eu pensei {computaddor} e você chutou o número {numero_chutado}.')
+    print('------------ Chute outro número ------------\n')
+    contador += 1
+    
+print(f'PARABÉNS! Você chutou o número {numero_chutado} e eu pensei {computaddor}.')
+print(f'Você chutou {contador} número(s).')
+print('----------------- VOCÊ VENCEU -----------------\n')
+    
