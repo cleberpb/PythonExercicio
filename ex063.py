f6b1f9ebcac57469c2e27103cf5e61326258243a
cleberpb <cleberppio@gmail.com>
2021-07-26 16:47:54 -0300

    Tentei

--- ex063.py
+++ ex063.py
@@ -4,3 +4,13 @@ na tela os n primeiros elemento de uma Sequência de Fibonacci.
 Ex:
 0 ⇾ 1 ⇾ 1 ⇾ 2 ⇾ 3 ⇾ 5 ⇾ 8
 '''
+
+numero = 0
+elementos = 0
+#soma = 0
+
+while numero < elementos:
+    numero = int(input('Digite um número: '))
+    elementos = int(input('Digite quantos elementos da Seguência de Fibonacci: '))
+    soma = numero + numero
+    print(soma)
