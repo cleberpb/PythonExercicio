4e5557edadeec33acd96c45bc7700efed55b36c7
cleberpb <36317652+cleberpb@users.noreply.github.com>
2021-07-26 14:00:45 -0300

    Fatorial

--- ex060.py
+++ ex060.py
@@ -3,3 +3,14 @@ Faça um programa que leia um número qualquer e mostre o seu fatorial.
 Ex:
 5! = 5x4x3x2x1 = 120
 '''
+
+import math
+
+
+numero = 0
+while numero >= 0 :
+    numero = int(input('\nDigite um número inteiro: '))
+    fatorial = math.factorial(numero)
+    print(f'O fatorial de {numero} é {fatorial}.')
+    print('-='*15)    
+print('Você saiu do programa')
